0a41a765a677b3bbcd2e15c4fe416b7a51b6e49f
Timshel <Timshel@users.noreply.github.com>
2023-12-21 18:24:46 +0100

    Upgrade to v2023.12.0

--- build_versions.sh
+++ build_versions.sh
@@ -5,8 +5,8 @@ set -e
 SHORT_COMMIT_HASH=$(git rev-parse --short HEAD)
 
 # Commit to fetch
-VAULT_VERSION=web-v2023.10.0
-VW_VERSION=v2023.10.0
+VAULT_VERSION=web-v2023.12.0
+VW_VERSION=v2023.12.0
 
 # VaultWarden patch to apply
 PATCH_NAME="$VW_VERSION"
