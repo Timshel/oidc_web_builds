4a727ec851151b6cff0bcd6e55eb3baa1c1d64e2
Timshel <Timshel@users.noreply.github.com>
2024-01-24 09:08:30 +0100

    Upgrade to v2024.1.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## v2024.1.2
+
+ - Upgrade to `v2024.1.2`
+
 ## v2024.1.1-1
 
  - Upgrade to `v2024.1.1`
--- build_versions.sh
+++ build_versions.sh
@@ -5,9 +5,8 @@ set -e
 SHORT_COMMIT_HASH=$(git rev-parse --short HEAD)
 
 # Commit to fetch
-VAULT_VERSION=web-v2024.1.1
-VW_VERSION=v2024.1.1b
+VAULT_VERSION=web-v2024.1.2
+VW_VERSION=v2024.1.2
 
 # VaultWarden patch to apply
-# PATCH_NAME="$VW_VERSION"
-PATCH_NAME="v2024.1.0"
+PATCH_NAME="$VW_VERSION"
